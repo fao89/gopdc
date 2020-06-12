4952160e69e1c3e84f23ea2ed311e1ceb996bef1
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-06-12 12:44:57 -0300

    Going async

--- pdc.go
+++ pdc.go
@@ -13,15 +13,17 @@ import (
 	"github.com/Masterminds/semver"
 )
 
+// PypiData : stores the desired PyPI data
 type PypiData struct {
+	Name     string
 	Version  string
 	Requires string
 	Releases []string
 }
 
 func main() {
-	pypi_url := "https://pypi.org/pypi/pulpcore/json"
-	pulp_plugins := []string{
+	pypiURL := "https://pypi.org/pypi/pulpcore/json"
+	pulpPlugins := []string{
 		"galaxy-ng",
 		"pulp-ansible",
 		"pulp-certguard",
@@ -35,19 +37,29 @@ func main() {
 		"pulp-python",
 		"pulp-rpm",
 	}
-	pulpcore_data := getPypiData(pypi_url)
-	for index := range pulpcore_data.Releases {
-		pulpcore_version := pulpcore_data.Releases[len(pulpcore_data.Releases)-1-index]
-		if strings.Contains(pulpcore_version, "3.0.0") {
+	channel := make(chan PypiData)
+	go getPypiData(pypiURL, channel)
+	pulpcoreData := <-channel
+
+	for _, plugin := range pulpPlugins {
+		go getPypiData(strings.Replace(pypiURL, "pulpcore", plugin, -1), channel)
+	}
+	pulpPluginsData := make([]PypiData, len(pulpPlugins))
+	for index := range pulpPlugins {
+		pulpPluginsData[index] = <-channel
+	}
+	for index := range pulpcoreData.Releases {
+		pulpcoreVersion := pulpcoreData.Releases[len(pulpcoreData.Releases)-1-index]
+		if strings.Contains(pulpcoreVersion, "3.0.0") {
 			// avoiding rc versions
-			printCompatiblePlugins(pypi_url, "3.0.0", pulp_plugins)
+			printCompatiblePlugins(pypiURL, "3.0.0", pulpPluginsData)
 			break
 		}
-		printCompatiblePlugins(pypi_url, pulpcore_version, pulp_plugins)
+		printCompatiblePlugins(pypiURL, pulpcoreVersion, pulpPluginsData)
 	}
 }
 
-func getPypiData(url string) *PypiData {
+func getPypiData(url string, channel chan PypiData) {
 	var myClient = &http.Client{Timeout: 10 * time.Second}
 	r, err := myClient.Get(url)
 	if err != nil {
@@ -63,6 +75,7 @@ func getPypiData(url string) *PypiData {
 
 	itemsMap := data.(map[string]interface{})
 	info := itemsMap["info"].(map[string]interface{})
+	name := fmt.Sprintf("%v", info["name"])
 	version := fmt.Sprintf("%v", info["version"])
 	releasesMap := itemsMap["releases"].(map[string]interface{})
 	releasesInterface := reflect.ValueOf(releasesMap).MapKeys()
@@ -81,32 +94,32 @@ func getPypiData(url string) *PypiData {
 		}
 	}
 
-	return &PypiData{version, requires, releases}
+	channel <- PypiData{name, version, requires, releases}
 }
 
-func printCompatiblePlugins(pypi_url string, pulpcore_version string, plugins []string) {
+func printCompatiblePlugins(pypiURL string, pulpcoreVersion string, plugins []PypiData) {
 	shown := false
-	for index, plugin := range plugins {
-		if plugin == "remove" {
+	for index, pypiData := range plugins {
+		if pypiData.Name == "remove" {
 			continue
 		}
-		pypi_data := getPypiData(strings.Replace(pypi_url, "pulpcore", plugin, -1))
-		req := strings.Fields(pypi_data.Requires)
+		req := strings.Fields(pypiData.Requires)
 		c, err := semver.NewConstraint(strings.Replace(req[1], "~=", "~", -1))
 		if err != nil {
 			c, _ = semver.NewConstraint("<3.0.1")
 		}
-		v, err := semver.NewVersion(pulpcore_version)
+		v, err := semver.NewVersion(pulpcoreVersion)
 		if err != nil {
 			panic(err.Error())
 		}
 		if c.Check(v) {
 			if !shown {
-				fmt.Println(fmt.Sprintf("\nCompatible with pulpcore-%s", pulpcore_version))
+				fmt.Println(fmt.Sprintf("\nCompatible with pulpcore-%s", pulpcoreVersion))
 				shown = true
 			}
-			fmt.Println(fmt.Sprintf(" -> %s-%s requirement: %s", plugin, pypi_data.Version, pypi_data.Requires))
-			plugins[index] = "remove"
+			fmt.Println(fmt.Sprintf(" -> %s-%s requirement: %s", pypiData.Name, pypiData.Version, pypiData.Requires))
+			pypiData.Name = "remove"
+			plugins[index] = pypiData
 		}
 	}
 }
